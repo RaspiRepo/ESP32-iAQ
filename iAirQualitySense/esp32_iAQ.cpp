1be38132195ed3c3ef71c0da13cd6c708fabbf66
Raspirepo <mariya.k@live.com>
2021-01-29 01:45:22 -0800

    added lightning sensor reading

--- iAirQualitySense/esp32_iAQ.cpp
+++ iAirQualitySense/esp32_iAQ.cpp
@@ -15,6 +15,8 @@ esp32_iAQ::esp32_iAQ()
   humidity         = 0.0;
   indoor_co2_level = 0;
   resistance       = 0; 
+  afe_mode = " Indoor ";
+  event_info = "";
 }
 
 esp32_iAQ::~esp32_iAQ()
@@ -26,7 +28,7 @@ esp32_iAQ::~esp32_iAQ()
 {
 }
 
-void esp32_iAQ::init_ENS210(uint8_t deviceID)
+void esp32_iAQ::init_ENS210()
 /*----------------------------------------------------------------------------
     init_ENS210 : Initialize and start Temp/Hum senssor ENS210. Refer Datasheet 
                   for more details 
@@ -34,10 +36,27 @@ void esp32_iAQ::init_ENS210(uint8_t deviceID)
     Written by  : RaspiRepo
 ------------------------------------------------------------------------------*/
 {
-  write2device(deviceID, (uint8_t)ENS210_SENS_RUN);
-  write2device(deviceID, (uint8_t)0x3);
-  write2device(deviceID, (uint8_t)0x3);
+  Wire.beginTransmission(ENS210_I2C_DEVICE_ADDRESS);
+  Wire.write((uint8_t)ENS210_SENS_RUN);
+  Wire.write((uint8_t)0x0);
+  Wire.write((uint8_t)0x0);
+  Wire.endTransmission();   
 }
+
+void esp32_iAQ::start_ENS210()
+/*----------------------------------------------------------------------------
+    init_ENS210 : Initialize and start Temp/Hum senssor ENS210. Refer Datasheet 
+                  for more details 
+                  
+    Written by  : RaspiRepo
+------------------------------------------------------------------------------*/
+{
+  Wire.beginTransmission(ENS210_I2C_DEVICE_ADDRESS);
+  Wire.write((uint8_t)ENS210_SENS_START);
+  Wire.write((uint8_t)0x03);
+  Wire.endTransmission();
+}
+
 
 float esp32_iAQ::get_temperature()
 /*----------------------------------------------------------------------------
@@ -118,7 +137,10 @@ void esp32_iAQ::read_from_ens210 ()
   uint8_t bytes_ret = 0; 
   uint8_t *data_buf = ens210_data_buf; 
 
-  //set data register of sensor
+  //single shot reading
+  start_ENS210();
+
+  //set data register of sensor for reading
   write2device(ENS210_I2C_DEVICE_ADDRESS, ENS210_DATA_REGISTER);
 
   // wait for bytes from slave device and add received bytes to buffer
@@ -140,7 +162,6 @@ void esp32_iAQ::read_from_ens210 ()
   
   temperature = (float)(TinC * 1.8f + 32.0f); // Temperature in Fahrenheit
 
-  
   uint32_t h_data = (h_val>>0 ) & 0xffff;
   /*
   uint32_t h_valid= (h_val>>16) & 0x1;
@@ -178,3 +199,145 @@ uint8_t esp32_iAQ::read_from_iAQc ()
     }
     return status;
 }
+
+
+
+void esp32_iAQ::lightning_sensor_start ()
+/*----------------------------------------------------------------------------
+    lightning_sensor_start:
+                          Initialize Lightning Sensor AS3935
+    Written By          : RaspiRepo (mariya.k@gmail.com)
+                          https://github.com/RaspiRepo
+    Address             : Mountain View, CA 94040
+    Date                : May 2017
+------------------------------------------------------------------------------*/
+{
+  Wire.beginTransmission(AS3936_LIGHTING_SENSOR_ADDRESS);
+  Wire.write((uint8_t)0x3c);
+  Wire.write((uint8_t)0x96);
+  Wire.write((uint8_t)0x96);
+  Wire.endTransmission(); 
+}
+
+
+String esp32_iAQ::get_afe_sensor_mode ()
+/*----------------------------------------------------------------------------
+    get_afe_sensor_mode:
+                          AS3935 Franklin Lightning Sensor IC: Lightning sensor
+                          warns of lightning storm activity within a radius
+                          of 40km
+    Written By          : RaspiRepo (mariya.k@gmail.com)
+                          https://github.com/RaspiRepo
+    Address             : Mountain View, CA 94040
+    Date                : May 2017
+------------------------------------------------------------------------------*/
+{   
+  return afe_mode;
+}
+
+String esp32_iAQ::get_lighting_event ()
+/*----------------------------------------------------------------------------
+    get_lighting_event:
+                          AS3935 Franklin Lightning Sensor IC: Lightning sensor
+                          warns of lightning storm activity within a radius
+                          of 40km
+    Written By          : RaspiRepo (mariya.k@gmail.com)
+                          https://github.com/RaspiRepo
+    Address             : Mountain View, CA 94040
+    Date                : May 2017
+------------------------------------------------------------------------------*/
+{   
+  return event_info;
+}
+
+
+uint8_t esp32_iAQ::lightning_sensor_reading ()
+/*----------------------------------------------------------------------------
+    lightning5_sensor_reading:
+                          AS3935 Franklin Lightning Sensor IC: Lightning sensor
+                          warns of lightning storm activity within a radius
+                          of 40km
+    Written By          : RaspiRepo (mariya.k@gmail.com)
+                          https://github.com/RaspiRepo
+    Address             : Mountain View, CA 94040
+    Date                : May 2017
+------------------------------------------------------------------------------*/
+{
+    //Statistical Distance Estimation:
+    //The AS3935 generates an assessment of the estimated distance to the head of an approaching storm.
+    //This assessment is done based on statistical calculation. The energy of the single event (lightning) 
+    //provided by the Energy Calculation block is stored in an internal memory, together with timing
+    //information, in the AS3935. The events stored in the memory are then correlated with a look-up table by the statistical
+    //distance estimation block, which provides a final estimation of the distance to the head of the storm. 
+    //The algorithm automatically deletes events, which are older than a certain time. R7=0x01 means that 
+    //the storm is right overhead, while R7=0x3F is displayed when the storm is out of range.
+    //This algorithm is hardwired and not accessible from outside.
+    //The estimated distance is directly represented in km in the register REG0x07[5:0] (binary encoded).
+    //The distance estimation can change also if no new event triggers the AS3935, as older events can be purged.
+
+    uint8_t bytes_ret = 0; 
+    uint8_t *data_buf = as3935_data_buf; 
+    event_info = "";
+      
+    //Issue read command
+    write2device(AS3936_LIGHTING_SENSOR_ADDRESS, AS3935_AFE_REG);
+        
+    //requesr 9 bytes only 3 bytes used here. Details on Datasheet
+    Wire.requestFrom(AS3936_LIGHTING_SENSOR_ADDRESS, 0x08);
+    
+    // wait for bytes from slave device and add received bytes to buffer
+    //max 6 bytes request
+    while (Wire.available() && bytes_ret < 0x08) { 
+      *data_buf = Wire.read();
+      ++data_buf;
+      ++bytes_ret;
+    }
+    
+    //REG0x00[5:1]   - 0x24 - means indoor :  10010
+    //Outdoor 01110  - 0x1c = means outdoor
+    afe_setting = as3935_data_buf[0] & 0x3E;
+
+    if (afe_setting == 0x1c) {
+      afe_mode = " Outdoor ";
+    }
+    
+    uint8_t noise_floor_level = NoiseFloorThreshold[(as3935_data_buf[1] & 0x70 >> 4)];
+    uint8_t detection_efficiency = as3935_data_buf[2] & 0x0F;
+
+    uint32_t energy = (as3935_data_buf[6] & 0x1f) << 0x08;
+    energy = energy | (as3935_data_buf[5] << 0x08);
+    energy = energy | (uint8_t)(as3935_data_buf[4]);
+
+    uint8_t distance = as3935_data_buf[7] & 0x3f;
+
+    //even information deom sensor
+    uint8_t intrpt_event = as3935_data_buf[3] & 0x0f;
+    if (intrpt_event & 0x01) {
+      event_info = " Noise level too high ";
+    } else if (intrpt_event & 0x04) {
+      event_info = " Disturber detected ";
+    } else if (intrpt_event & 0x08) {
+      event_info = " Lightning interrupt ";
+    }
+    
+//    Serial.print(" Mode ");
+//    Serial.print(afe_setting);
+//    Serial.print(" NseFLvel ");
+//    Serial.print(noise_floor_level);
+//    Serial.print(" DF ");
+//    Serial.print(detection_efficiency);   
+//    Serial.print(" ERGY ");
+//    Serial.print(energy);   
+//    Serial.print(" Distance ");
+//    if (distance == 0x3f) {
+//      Serial.print(" Out of range ");
+//    } else if (distance == 0x01) {
+//      Serial.print("  Storm is Overhead ");
+//    } else {
+//      Serial.print(distance);
+//    }
+//    
+
+    return distance;
+
+} 
--- iAirQualitySense/esp32_iAQ.h
+++ iAirQualitySense/esp32_iAQ.h
@@ -6,9 +6,15 @@
 
 #define ENS210_I2C_DEVICE_ADDRESS  0x43
 #define ENS210_SENS_RUN            0x21
+#define ENS210_SENS_START          0x22
 #define ENS210_DATA_REGISTER       0x30
-
 #define iAQ_I2C_DEVICE_ADDRESS     0x5A
+
+//Lighting sensor
+#define AS3936_LIGHTING_SENSOR_ADDRESS 0x02
+#define AS3935_AFE_REG                 0x00
+#define AS3935_ESTIMATED_DISTANCE_REG  0x07
+
 
 class esp32_iAQ
 /*----------------------------------------------------------------------------
@@ -23,13 +29,20 @@ class esp32_iAQ
     ~esp32_iAQ();
 
     //to issue start command to ENS210
-    void init_ENS210(uint8_t deviceID);
+    void init_ENS210();
+    void start_ENS210();
 
     //public functions to get current sensor's value
     float  get_temperature();
     float  get_humidity();
     uint16_t get_iaq();
 
+    void lightning_sensor_start ();
+    //return distance in km
+    uint8_t lightning_sensor_reading();
+    String get_afe_sensor_mode ();
+    String get_lighting_event ();
+    
   private:
     uint8_t deviceID;
     float  temperature;
@@ -37,10 +50,18 @@ class esp32_iAQ
     uint16_t  indoor_co2_level;
     uint16_t  resistance;
 
+    uint8_t afe_setting;
+    uint8_t est_distance;
+    float g_lightning_miles;
+    String afe_mode;
+    String event_info;
+
     //micro controler chip ID
     String McChipID;
 
     uint8_t ens210_data_buf[6] = { 0x0,0x0,0x0,0x0,0x0,0x0 };
+    uint8_t as3935_data_buf[9] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
+    uint8_t NoiseFloorThreshold[8] = {28, 45, 62, 78, 95, 112, 130, 146};
 
     //reading functions
     void read_from_ens210 ();
